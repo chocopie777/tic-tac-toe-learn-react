c928cd4e2710f82367be269f31a9c6d2a084991b
chocopie777 <egoran99@gmail.com>
2023-08-30 20:42:13 +0500

    commit2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import {useState} from "react";
 import './App.css';
 
 export default function Board() {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
+        if(squares[i] || calculateWinner(squares)) {
+            return;
+        }
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = 'O';
+        }
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
 
     return (
@@ -33,4 +42,24 @@ export default function Board() {
 
 function Square({value, onSquareClick}) {
     return <button className="square" onClick={onSquareClick}>{value}</button>
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for (let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
